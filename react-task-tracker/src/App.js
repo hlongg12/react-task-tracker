d74214f01a9ce297455bee6a21f23c0ddb8615ee
hlongg12 <hlongg12@gmail.com>
2021-05-21 23:32:01 +1000

    Add task form and task submit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask"
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,6 +26,13 @@ function App() {
     },
   ]);
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 100000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -40,7 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : 'No tasks to show'}
     </div>
   );
